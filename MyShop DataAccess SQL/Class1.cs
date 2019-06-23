3eb1361980eecb20efa4f625de080f4ba0977244
Damjan <jasamprogramer7@gmail.com>
2019-06-23 23:26:54 +0100

    Initial Solution Setup

---
+++ MyShop DataAccess SQL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop_DataAccess_SQL
+{
+    public class Class1
+    {
+    }
+}
