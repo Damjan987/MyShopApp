c9d987afb49f4cff3cd1c472e4ceb95e7dbf31a8
Damjan <jasamprogramer7@gmail.com>
2019-06-25 11:29:56 +0100

    Product Grouping adding

--- MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -5,16 +5,19 @@ using System.Web;
 using System.Web.Mvc;
 using MyShop_Core.Models;
 using MyShop.DataAccess.InMemory;
+using MyShop_Core.ViewModels;
 
 namespace MyShop.WebUI.Controllers
 {
     public class ProductManagerController : Controller
     {
         ProductRepository context;
+        ProductCategoryRepository productCategories;
 
         public ProductManagerController()
         {
             context = new ProductRepository();
+            productCategories = new ProductCategoryRepository();
         }
 
         // GET: ProductManager
@@ -25,8 +28,11 @@ namespace MyShop.WebUI.Controllers
         }
 
         public ActionResult Create() {
-            Product product = new Product();
-            return View(product);
+            ProductManagerViewModel viewModel = new ProductManagerViewModel();
+
+            viewModel.Product = new Product();
+            viewModel.ProductCategories = productCategories.Collection();
+            return View(viewModel);
         }
 
         [HttpPost]
@@ -50,7 +56,11 @@ namespace MyShop.WebUI.Controllers
             }
             else
             {
-                return View(product);
+                ProductManagerViewModel viewModel = new ProductManagerViewModel();
+                viewModel.Product = product;
+                viewModel.ProductCategories = productCategories.Collection();
+
+                return View(viewModel);
             }
         }
 
